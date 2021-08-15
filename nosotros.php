adaa96e302ed2f4c1d817f2d398c252bb88a0055
Manu <manuelchavarin@aori.mx>
2021-08-15 13:36:38 -0500

    slider antes despues

--- nosotros.php
+++ nosotros.php
@@ -3,13 +3,33 @@
 <head>
     <?php include('partial_views/head_tag.php') ?>
     <script src="/assets/scripts/img-slider.js"></script>
-    <title>Schedia - Interiorismo corporativo</title>
+    <title>Schedia - Nosotros</title>
 </head>
 <body>
-    <div class="slider-images">
-        <div class="slider-image back-img"></div>
-        <div class="slider-image front-img"></div>
-        <input id="rangeIMG" type="range" min="0" max="100" step="1">
+    <?php include('partial_views/navbar.php') ?>
+    <div class="page-hero nosotros-hero">
+        <div class="gmw">
+            <h1>
+                <span>Nosotros</span>
+                <span>Apasionados del interiorismo corporativo</span>
+            </h1>
+            <a class="main-button left-desktop-no-mobile"><span>Contactar</span></a>
+        </div>
     </div>
+
+    <div class="main-wrapper mision-wrapper">
+        <div class="gmw">
+            <div class="info">
+                <h2 class="main-title">
+                    <span>Nuestra misión</span>
+                    <span>Apasionados por crear los mejores espacios</span>
+                </h2>
+                <p class="text-box mt-2">Somos una empresa de construcción enfocada y especializada en ejecución de interiores corporativos. En conjunto con la mano de obra especializada en cada ramo, realizamos un seguimiento puntual de los procesos para ofrecer calidad en entrega y forma, sin sobrecostos ni órdenes de cambio no solicitadas por el cliente final.</p>
+                <button class="main-button black-button mt-3 left-desktop-no-mobile"><span>Ver proyectos</span></button>
+            </div>
+            <?php include('partial_views/img-slider.php') ?>
+        </div>
+    </div>
+    <?php include('partial_views/footer.php') ?>
 </body>
 </html>
---
+++ partial_views/img-slider.php
@@ -0,0 +1,12 @@
+<div class="slider-images">
+    <img class="slider-image front-img" src="/assets/images/nosotros/antes.jpg" alt="">
+    <img class="slider-image back-img" src="/assets/images/nosotros/despues.jpg" alt="">
+    <div id="icons" class="icon-divider">
+        <i class="fas fa-caret-left"></i>
+        <i class="fas fa-caret-right"></i>
+    </div>
+    <div class="top divider"></div>
+    <div class="bottom divider"></div>
+
+    <input id="rangeIMG" type="range" min="0" max="100" step="1">
+</div>
