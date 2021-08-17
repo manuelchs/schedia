b0e3728b7ade740a2879b6adc0fc2b24b51a187e
Manu <manuelchavarin@aori.mx>
2021-08-16 19:10:20 -0500

    Avance a nosotros

--- nosotros.php
+++ nosotros.php
@@ -30,6 +30,80 @@
             <?php include('partial_views/img-slider.php') ?>
         </div>
     </div>
+
+    <div class="main-wrapper soluciones-wrapper">
+        <div class="gmw">
+            <div class="photo-text-container">
+                <div class="photo"></div>
+                <div class="text">
+                    <h2 class="main-title">
+                        <span>Soluciones integrales</span>
+                        <span>Del concepto a la realidad</span>
+                    </h2>
+                    <p class="mt-2 text-box">Siempre buscamos un valor agregado en los proyectos que realizamos haciendo un estudio global en búsqueda de optimizar en todo momento los aspectos logísticos de obra y financieros que ayuden a nuestros clientes a consolidar sus proyectos con calidad y dentro de los parámetros económicos requeridos siempre en tiempo.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="main-wrapper gray-wrapper elegirnos-wrapper">
+        <div class="gmw">
+            <h2 class="main-title centered">
+                <span>¿Por qué elegirnos?</span>
+                <span>El valor de trabajar con nostros.</span>
+            </h2>
+            <div class="grid-icons">
+                <div class="grid-icon-item">
+                    <div class="icon-wrapper">
+                        <img class="normal" src="/assets/icons/elegirnos/tiempo-de-reaccion-reducido.png" alt="">
+                        <img class="hover" src="/assets/icons/elegirnos/tiempo-de-reaccion-reducido-hover.png" alt="">
+                    </div>
+                    <h3>Tiempo de reacción reducido</h3>
+                    <p class="text-box">Mano de obra basada en empresas locales, lo que hace que nuestro tiempo de reacción sea mucho menor.</p>
+                </div>
+                <div class="grid-icon-item">
+                    <div class="icon-wrapper">
+                        <img class="normal" src="/assets/icons/elegirnos/atencion-247.png" alt="">
+                        <img class="hover" src="/assets/icons/elegirnos/atencion-247-hover.png" alt="">
+                    </div>
+                    <h3>Atención<br>24/7</h3>
+                    <p class="text-box">Durante todo el proyecto ponemos a disposición del cliente un encargado (PM SR. Bilingüe) 24/7 para aclaración de dudas.</p>
+                </div>
+                <div class="grid-icon-item">
+                    <div class="icon-wrapper">
+                        <img class="normal" src="/assets/icons/elegirnos/seguimiento-del-proyecto.png" alt="">
+                        <img class="hover" src="/assets/icons/elegirnos/seguimiento-del-proyecto-hover.png" alt="">
+                    </div>
+                    <h3>Seguimiento del proyecto</h3>
+                    <p class="text-box">Ponemos un PM JR por cada 1000 m2 para el correcto seguimiento del proyecto, así como un residente de acabados por cada 1000 m2 o menos.</p>
+                </div>
+                <div class="grid-icon-item">
+                    <div class="icon-wrapper">
+                        <img class="normal" src="/assets/icons/elegirnos/seguridad-e-higiene.png" alt="">
+                        <img class="hover" src="/assets/icons/elegirnos/seguridad-e-higiene-hover.png" alt="">
+                    </div>
+                    <h3 class="small">Seguridad e higiene</h3>
+                    <p class="text-box">Durante todo proyecto disponemos a un encargado de seguridad e higiene por cada 500 m2, siguiendo todos los protocolos anti-COVID y empleando filtros sanitarios.</p>
+                </div>
+                <div class="grid-icon-item">
+                    <div class="icon-wrapper">
+                        <img class="normal" src="/assets/icons/elegirnos/fianzas-de-cumplimiento.png" alt="">
+                        <img class="hover" src="/assets/icons/elegirnos/fianzas-de-cumplimiento-hover.png" alt="">
+                    </div>
+                    <h3>Fianzas de Cumplimiento</h3>
+                    <p class="text-box">Le otorgamos a nuestros clientes hasta 3 años de garantía, brindándoles el respaldo adecuado por medio de fianzas de cumplimiento, buen uso, responsabilidad civil y vicios ocultos.</p>
+                </div>
+                <div class="grid-icon-item">
+                    <div class="icon-wrapper">
+                        <img class="normal" src="/assets/icons/elegirnos/seguimiento-postconstruccion.png" alt="">
+                        <img class="hover" src="/assets/icons/elegirnos/seguimiento-postconstruccion-hover.png" alt="">
+                    </div>
+                    <h3>Seguimiento post construcción 24/7</h3>
+                    <p class="text-box">Equipo de seguimiento de post construcción 24/7 con un tiempo de reacción menor a 30 minutos.</p>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php include('partial_views/footer.php') ?>
 </body>
 </html>
