15094c61e74f13f23dd6809ba1b459ec8a09ea49
Manu <manuelchavarin@aori.mx>
2021-09-12 23:57:08 -0500

    betterware

--- en/partial_views/proyectos/betterware.php
+++ en/partial_views/proyectos/betterware.php
@@ -1,15 +1,91 @@
 <div class="floating-item left-bg">
     <div class="bg-img custom-animation slow-animation custom-animation-bottom">
-        <img src="/assets/images/portafolio/Avnet/avnet-1.jpeg" alt="">
+        <img src="/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_1.jpg" alt="">
         <div class="overlay">
         </div>
     </div>
-    <div class="logo-wrapper custom-animation slow-animation custom-animation-left">
+    <div class="logo-wrapper custom-animation slow-animation custom-animation-left"  onclick="openModal('better')">
         <div class="logo">
             <img src="/assets/images/clientes/betterware-big.png" alt="">
         </div>
         <div class="button-section">
-            <button><span>VIEW GALLERY</span><i class="far fa-long-arrow-alt-right"></i></button>
+            <button><span>View gallery</span><i class="far fa-long-arrow-alt-right"></i></button>
+        </div>
+    </div>
+</div>
+
+<div id="better" class="modal">
+    <div class="close-modal"><i class="fal fa-times"></i></div>
+    <div class="slider-wrapper proyectos-slider">
+        <div class="slider-button slider-button-left">
+            <i class="far fa-chevron-left"></i>
+        </div>
+        <div class="slider-button slider-button-right">
+            <i class="far fa-chevron-right"></i>
+        </div>
+        <div id="sliderBetter" class="slider column">
+            <div class="slider-item s1">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_1.jpg')"></div>
+            </div>
+            <div class="slider-item s2">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_37.jpg')"></div>
+            </div>
+            <div class="slider-item s3">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_418.jpg')"></div>
+            </div>
+            <div class="slider-item s4">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_462.jpg')"></div>
+            </div>
+            <div class="slider-item s5">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_560.jpg')"></div>
+            </div>
+            <div class="slider-item s6">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_561.jpg')"></div>
+            </div>
+            <div class="slider-item s7">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_628.jpg')"></div>
+            </div>
+            <div class="slider-item s8">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_666.jpg')"></div>
+            </div>
+            <div class="slider-item s9">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_735.jpg')"></div>
+            </div>
+            <div class="slider-item s10">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_766.jpg')"></div>
+            </div>
+            <div class="slider-item s11">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_770.jpg')"></div>
+            </div>
+            <div class="slider-item s12">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_813.jpg')"></div>
+            </div>
+            <div class="slider-item s13">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_90.jpg')"></div>
+            </div>
+            <div class="slider-item s14">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_915.jpg')"></div>
+            </div>
+            <div class="slider-item s15">
+                <div class="img-wrapper" style="background-image: url('/assets/images/portafolio/betterware/2021_05_23_SCHEDIA.BETTERWARE_9_918.jpg')"></div>
+            </div>
+        </div>
+        <div class="points">
+            <div class="points-item p1 active" div="1"></div>
+            <div class="points-item p2" div="2"></div>
+            <div class="points-item p3" div="3"></div>
+            <div class="points-item p4" div="4"></div>
+            <div class="points-item p5" div="5"></div>
+            <div class="points-item p6" div="6"></div>
+            <div class="points-item p7" div="7"></div>
+            <div class="points-item p8" div="8"></div>
+            <div class="points-item p9" div="9"></div>
+            <div class="points-item p10" div="10"></div>
+            <div class="points-item p11" div="11"></div>
+            <div class="points-item p12" div="12"></div>
+            <div class="points-item p13" div="13"></div>
+            <div class="points-item p14" div="14"></div>
+            <div class="points-item p15" div="15"></div>
         </div>
     </div>
 </div>
