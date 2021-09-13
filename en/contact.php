ce73b37c2bd1dfbb8307b3ac301ae6d8a5b3834f
Manu <manu.chs18@gmail.com>
2021-09-12 22:36:21 -0500

    Contacto

--- en/contact.php
+++ en/contact.php
@@ -3,25 +3,25 @@
 <head>
     <?php include('partial_views/head_tag.php') ?>
     <script src="/assets/scripts/contacto.js"></script>
-    <title>Schedia - Contacto</title>
+    <title>Schedia - Contact</title>
 </head>
 <body>
     <?php include('partial_views/navbar.php') ?>
     <div class="page-hero contacto-hero">
         <div class="gmw">
             <h1>
-                <span class="custom-animation custom-animation-bottom">Contacto</span>
-                <span class="custom-animation custom-animation-top">Tu proyecto comienza aquí</span>
+                <span class="custom-animation custom-animation-bottom">Contact</span>
+                <span class="custom-animation custom-animation-top">Your workspace starts here.</span>
             </h1>
-            <a href="tel:+553333080076" class="main-button left-desktop-no-mobile icon icon-left custom-animation custom-animation-bottom"><i class="fas fa-phone"></i><span>Llamar</span></a>
+            <a href="tel:+553333080076" class="main-button left-desktop-no-mobile icon icon-left custom-animation custom-animation-bottom"><i class="fas fa-phone"></i><span>Call us</span></a>
         </div>
     </div>
 
     <div class="main-wrapper contacto-wrapper">
         <div class="gmw">
             <div class="address-wrapper">
-                <h2 class="custom-animation custom-animation-bottom">Contáctanos</h2>
-                <p class="text-box custom-animation custom-animation-top">Cuéntanos cuál es el proyecto que te interesa. Cualquiera que sea tu sueño, nuestra pasión es ayudarte a descubrir el diseño exclusivo que lo hará realidad.</p>
+                <h2 class="custom-animation custom-animation-bottom">Get in touch</h2>
+                <p class="text-box custom-animation custom-animation-top">Tell us more about your project. No matter what you have in mind, our job is to make your workspace idea a reality.</p>
                 <a href="https://goo.gl/maps/S5GQT99cGXXP5eM98" target="_blank" class="address-item custom-animation custom-animation-left">
                     <i class="fal fa-map-marker-alt"></i>
                     <div class="data">
@@ -38,23 +38,23 @@
                 </a>
             </div>
             <div class="form-wrapper">
-                <h3 class="custom-animation custom-animation-bottom">Envíanos un mensaje</h3>
+                <h3 class="custom-animation custom-animation-bottom">Send us a message.</h3>
                 <form id="contact-form" action="sendemail.php" method="post">
-                    <input name="nombre" type="text" class="main-input custom-animation custom-animation-bottom" placeholder="Nombre*">
-                    <input name="email" type="text" class="main-input custom-animation custom-animation-top" placeholder="E-mail*">
-                    <input name="telefono" type="text" class="main-input custom-animation custom-animation-bottom" placeholder="Teléfono*">
+                    <input name="nombre" type="text" class="main-input custom-animation custom-animation-bottom" placeholder="Name*">
+                    <input name="email" type="text" class="main-input custom-animation custom-animation-top" placeholder="E-mail address*">
+                    <input name="telefono" type="text" class="main-input custom-animation custom-animation-bottom" placeholder="Phone number*">
                     <div class="main-select custom-animation custom-animation-top">
                         <select name="asunto">
-                            <option value="">Elige un asunto</option>
-                            <option value="Cotizaciones">Requiero una cotización</option>
-                            <option value="Sugerencias">Deseo agendar una cita o video llamada</option>
-                            <option value="Comentarios">Otro asunto</option>
+                            <option value="">Subject</option>
+                            <option value="Cotizaciones">Request proposal</option>
+                            <option value="Sugerencias">Schedule an appointment</option>
+                            <option value="Comentarios">Other subject</option>
                         </select>
                         <div><i class="fas fa-caret-down"></i></div>
                     </div>
-                    <textarea name="mensaje" class="main-input custom-animation custom-animation-bottom" placeholder="Escríbenos un mensaje"></textarea>
-                    <button id="contact-form-btn" type="submit" class="main-button black-button custom-animation custom-animation-top"><span>Enviar mensaje</span></button>
-                    <div class="terms custom-animation custom-animation-bottom">Al dar clic en "Enviar mensaje" aceptas nuestro <a>Aviso de privacidad</a>. Te aseguramos que no compartiremos tus datos con nadie.</div>
+                    <textarea name="mensaje" class="main-input custom-animation custom-animation-bottom" placeholder="Write your message, comment or suggestion"></textarea>
+                    <button id="contact-form-btn" type="submit" class="main-button black-button custom-animation custom-animation-top"><span>Send message</span></button>
+                    <div class="terms custom-animation custom-animation-bottom">Once you click the “send message” button, you automatically agree and accept our <a>privacy notice</a>. We assure you that all the information we receive from your behalf, will only be used for internal purposes and will not be shared with any third parties.</div>
                 </form>
             </div>
         </div>
